1e7b9100c825a5bb9f48e579d5d9c987c08779bb
liluyang1999 <liluyang1999@163.com>
2023-01-20 16:31:39 +0800

    Practice for java http server......

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,10 @@
 import com.sun.net.httpserver.HttpExchange;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Test {
